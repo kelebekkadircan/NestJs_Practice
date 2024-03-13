d00a21a3581909dad6830626e3e366896c6064e7
kelebekkadircan <kelebekkadircan@gmail.com>
2024-03-13 22:13:53 +0300

    typeOrm and mysql initialize

---
+++ nestjs_typeorm/src/users/controllers/users/users.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { UpdateUserDto } from 'src/users/dtos/UpdateUser.dto';
+import { UsersService } from 'src/users/services/users/users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  async getUsers() {
+    const users = await this.usersService.findUsers();
+    return users;
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
+  }
+
+  @Put(':id')
+  async updateUserById(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return await this.usersService.updateUser(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  async deleteUserById(@Param('id', ParseIntPipe) id: number) {
+    return await this.usersService.deleteUser(id);
+  }
+}
