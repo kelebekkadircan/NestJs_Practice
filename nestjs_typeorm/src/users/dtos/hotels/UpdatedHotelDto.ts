085e408b2ef3a20fa75b50b567ff41ec41bf141f
kelebekkadircan <kelebekkadircan@gmail.com>
2024-03-16 12:55:53 +0300

    dto update

--- nestjs_typeorm/src/users/dtos/hotels/UpdatedHotelDto.ts
+++ nestjs_typeorm/src/users/dtos/hotels/UpdatedHotelDto.ts
@@ -1,3 +1,4 @@
 import { CreateHotelDto } from './CreateHotels';
+import { PartialType } from '@nestjs/mapped-types';
 
-export class UpdatedHotelDto extends CreateHotelDto {}
+export class UpdatedHotelDto extends PartialType(CreateHotelDto) {}
