1f20419321978b21f0d0164bce0ba0897b96b012
kelebekkadircan <kelebekkadircan@gmail.com>
2024-03-23 17:56:55 +0300

    any

--- nestjs_typeorm/src/users/controllers/profile/profile.controller.ts
+++ nestjs_typeorm/src/users/controllers/profile/profile.controller.ts
@@ -17,7 +17,7 @@ export class ProfileController {
   constructor(private profileService: ProfileService) {}
 
   @Get()
-  async getProfiles() {
+  async getProfiles(asdasd: any) {
     return await this.profileService.getProfiles();
   }
 
